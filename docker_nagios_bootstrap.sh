121f86954740eff017b2921ee5466143012cc9a5
holmes6 <jwaltz666@gmail.com>
2019-11-21 18:47:10 +0000

    nagios2

--- docker_nagios_bootstrap.sh
+++ docker_nagios_bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt update
 apt-get -y install nagios-nrpe-server nagios-plugins
 sed 's/allowed_hosts=127.0.0.1/allowed_hosts=127.0.0.1,45.79.181.247/g' /etc/nagios/nrpe.cfg -i
 echo "command[check_disk]=/usr/lib/nagios/plugins/check_disk -w 20% -c 10% -p /dev/xvda1" >> /etc/nagios/nrpe.cfg
